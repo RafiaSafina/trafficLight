ab245b9d8c0ee88e509fc03246fa07317e4240cf
rafia <safinarafia@gmail.com>
2022-09-18 20:49:20 +0300

    setup corners

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        startButton.layer.cornerRadius = 10
     }
 
     @IBAction func startButtonDidTapped() {
