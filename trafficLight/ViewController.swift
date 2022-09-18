f2ec1656132014a10fa36718ff2d3fbdb49c15d5
rafia <safinarafia@gmail.com>
2022-09-18 23:23:00 +0300

    working with logic

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -13,17 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var startButton: UIButton!
+   
     
+    var trafficLightIndex = -1
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         redLight.layer.cornerRadius = redLight.frame.size.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
         startButton.layer.cornerRadius = 10
+        
     }
 
     @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        trafficLightIndex += 1
+        switch trafficLightIndex {
+        case 0: redLight.alpha = 1; greenLight.alpha = 0.3
+        case 1: yellowLight.alpha = 1; redLight.alpha = 0.3
+        case 2: greenLight.alpha = 1; yellowLight.alpha = 0.3
+        default: trafficLightIndex = 0;
+                 redLight.alpha = 1;
+                 greenLight.alpha = 0.3
+        }
     }
-    
 }
 
